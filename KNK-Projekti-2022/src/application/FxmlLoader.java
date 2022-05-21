6df5a87ae656e468e510a855ef2a5258cb864933
ZanaGuda <62018613+ZanaGuda@users.noreply.github.com>
2022-05-21 08:50:22 +0200

    wip

---
+++ KNK-Projekti-2022/src/application/FxmlLoader.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+
+import controllers.AppController;
+
+public class FxmlLoader {
+    private Pane view;
+    public Pane getPage(String fileName) {
+        try {
+            URL fileUrl = AppController.class.getResource("/views/" + fileName + ".fxml");
+            if (fileUrl == null ) {
+                throw new java.io.FileNotFoundException("FXML file can't be found");
+            }
+            view = new FXMLLoader().load(fileUrl);
+        }
+        catch(Exception e) {
+
+            e.printStackTrace();
+            System.out.println("fxml cant be found");
+        }
+        return view;
+    }
+}
