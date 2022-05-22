acf48fd333d5f7241e9078ea8d54bbc5a9deebf0
Shefketbylykbashi <shefketbylykbashi11@gmail.com>
2022-05-22 15:43:14 +0200

    wip

---
+++ KNK-Projekti-2022/src/controllers/AppointementsController.java
@@ -0,0 +1,215 @@
+package controllers;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import model.Appointment;
+import model.AppointmentSearchResult;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.sql.Date;
+import java.util.LinkedHashSet;
+import java.util.ResourceBundle;
+import java.util.function.Predicate;
+
+public class AppointementsController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TableView<AppointmentSearchResult> appsTable;
+    @FXML
+    private TableColumn<AppointmentSearchResult, Void> col_actions;
+    @FXML
+    private TableColumn<AppointmentSearchResult, String> col_fname;
+    @FXML
+    private TableColumn<AppointmentSearchResult, String> col_name;
+    @FXML
+    private TableColumn<AppointmentSearchResult, String> col_doc;
+    @FXML
+    private TableColumn<AppointmentSearchResult, String> col_adr;
+    @FXML
+    private TableColumn<AppointmentSearchResult, String> col_phoneN;
+    @FXML
+    private TextField search;
+    @FXML
+    private ComboBox<String> filter;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private DatePicker myDatePicker;
+    @FXML
+    private Button addNew;
+    private ObservableList<String> comboList = FXCollections.observableArrayList("All", "Ophtamologue", "Remplacant");
+    private ObservableList<AppointmentSearchResult> patientsList;
+    private FilteredList<AppointmentSearchResult> searchResultList =null;
+    private Callback<TableColumn<AppointmentSearchResult, Void>, TableCell<AppointmentSearchResult, Void>> cellFactory1;
+   // private Callback<TableColumn<AppointmentSearchResult, Void>, TableCell<AppointmentSearchResult, Void>> cellFactory2;
+    private LocalDate myDate;
+    private String dayFormat;
+    private Date dayFormatSql;
+    static boolean newAppointment=false;
+    boolean deleted=false;
+
+    public void addAppointements(ActionEvent event) throws IOException {
+        addNew.setDisable(true);
+        FXMLLoader fxmlload = new FXMLLoader(getClass().getResource("/views/addAppointements.fxml"));
+        root = (Parent) fxmlload.load();
+        addAppointmentController addAppointmentController = fxmlload.getController();
+        addAppointmentController.getDate(myDate,0);
+        stage = new Stage();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void chooseDate(ActionEvent event) {
+        myDate = myDatePicker.getValue();
+        dayFormat = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy"));
+        dayFormatSql = Date.valueOf(myDate.toString());
+        dateLabel.setText(dayFormat);
+        getIt();
+    }
+
+    public void getIt() {
+        int doc = 2;
+        if (filter.getValue() == "Ophtamologue") {
+            doc = 2;
+        } else if (filter.getValue() == "Remplacant") {
+            doc = 3;
+        }
+        if (filter.getValue() == null) {
+            patientsList = Appointment.search(dayFormatSql, "");
+        } else if (filter.getValue() == "All") {
+            patientsList = Appointment.search(dayFormatSql, "");
+        } else patientsList = Appointment.search(dayFormatSql, "", doc);
+        searchResultList = new FilteredList<AppointmentSearchResult>(patientsList, createPredicate(search.getText()));
+        appsTable.setItems(searchResultList);
+    }
+
+    private Predicate<AppointmentSearchResult> createPredicate(String searchText) {
+        return patient -> {
+            if (searchText == null || searchText.isEmpty()) {
+                return true;
+            }
+            String lowerCaseFilter = searchText.toLowerCase();
+            if (patient.getFirstName().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                return true;
+            } else if (patient.getSecondName().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                return true;
+            } else return false;
+        };
+    }
+    public void setCellFactory1() {
+        cellFactory1 = new Callback<TableColumn<AppointmentSearchResult, Void>, TableCell<AppointmentSearchResult, Void>>() {
+            @Override
+            public TableCell<AppointmentSearchResult, Void> call(final TableColumn<AppointmentSearchResult, Void> param) {
+                final TableCell<AppointmentSearchResult, Void> cell = new TableCell<AppointmentSearchResult, Void>() {
+                    Image imgd = new Image("/resources/DeleteBtn.png");
+                    ImageView del = new ImageView(imgd);
+                    private final Button dl = new Button("",del);
+
+                    {
+
+                        dl.setStyle(PatientsSceneController.style);
+                        
+                    }
+
+                    HBox container = new HBox(5,  dl);
+                    {
+                        dl.setOnAction((ActionEvent event) -> {
+                            AppointmentSearchResult patient = appsTable.getSelectionModel().getSelectedItem();
+                            if (patient == null) {
+                                Alert alert = new Alert(Alert.AlertType.ERROR, "No Selected Patient.");
+                                alert.showAndWait();
+                            }
+                            else {
+                                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want delete?");
+                                if (alert.showAndWait().get() == ButtonType.OK) {
+                                    Appointment.delete(patient.getRdv_id());
+                                    deleted=true;
+                                }
+                            }
+                        });
+                    }
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(container);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myDate = LocalDate.now();
+        myDatePicker.setValue(myDate);
+        dayFormat = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy"));
+        dayFormatSql = Date.valueOf(myDate.toString());
+        dateLabel.setText(dayFormat);
+        col_fname.setCellValueFactory(new PropertyValueFactory<AppointmentSearchResult, String>("secondName"));
+        col_name.setCellValueFactory(new PropertyValueFactory<AppointmentSearchResult, String>("firstName"));
+        col_adr.setCellValueFactory(new PropertyValueFactory<AppointmentSearchResult, String>("adr"));
+        col_phoneN.setCellValueFactory(new PropertyValueFactory<AppointmentSearchResult, String>("num"));
+        col_doc.setCellValueFactory(new PropertyValueFactory<AppointmentSearchResult, String>("doctor"));
+        setCellFactory1();
+        col_actions.setCellFactory(cellFactory1);
+        getIt();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Runnable updater = new Runnable() {
+                    @Override
+                    public void run() {
+                        if (newAppointment==true || deleted ==true) {
+                            if (newAppointment == true) {
+                                newAppointment=false;
+                                addNew.setDisable(false);
+                            }
+                            else deleted=false;
+                            getIt();
+                        }
+                    }
+                };
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ex) {
+                    }
+                    Platform.runLater(updater);
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+        search.setPromptText("Search ");
+        search.textProperty().addListener((Observable, oldValue, newValue) ->
+                searchResultList.setPredicate(createPredicate(newValue)));
+        filter.setItems(comboList);
+    }
+}
---
+++ KNK-Projekti-2022/src/controllers/addAppointmentController.java
@@ -0,0 +1,123 @@
+package controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import model.*;
+import model.Patient;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class addAppointmentController implements Initializable {
+    @FXML
+    private TextField search;
+
+    @FXML
+    private TableView<Patient> patientsTable;
+
+    @FXML
+    private TableColumn<Patient, String> col_fname;
+
+    @FXML
+    private TableColumn<Patient, String> col_name;
+
+    @FXML
+    private TableColumn<Patient, Date> col_bdate;
+
+    @FXML
+    private ComboBox<String> filter;
+
+    @FXML
+    private Button addButton;
+
+    private ObservableList<Patient> patientsList;
+    private FilteredList<Patient> searchResultList;
+    private SortedList<Patient> searchResultSortedList;
+    private LocalDate myDate;
+    private int mc;
+    private ObservableList<String> comboList = FXCollections.observableArrayList("Ophtamologue","Remplacant");
+    public void getDate (LocalDate Date, int k) {
+        myDate = Date;
+        mc = k;
+    }
+    public void addAppointment(ActionEvent event) throws IOException {
+        Patient patient = null;
+        patient = patientsTable.getSelectionModel().getSelectedItem();
+        if (patient == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No Selected Patient.");
+            alert.showAndWait();
+        }
+        else {
+            int doc = 2;
+            if (filter.getValue() == "Ophtamologue") {
+                doc = 2;
+            }
+            else if (filter.getValue() == "Remplacant") {
+                doc = 3;
+            }
+            if (mc == 0) {
+                Appointment Appoint = new Appointment(Date.valueOf(myDate.toString()), patient.getPatientId(), doc);
+                Appoint.add();
+                AppointementsController.newAppointment=true;
+            }
+            else if ( mc == 1) {
+                WaitingRoom wr = new WaitingRoom();
+                wr.add(patient.getPatientId(),doc);
+                WaitingRoomController.newAppointment=true;
+            }
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.close();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Appointment added sucessfully!");
+            alert.showAndWait();
+
+        }
+    }
+    public void getThemAll() {
+        patientsList = Patient.search("",1);
+        searchResultList = new FilteredList<>(patientsList, b -> true);
+        search.textProperty().addListener((Observable, oldValue, newValue) -> {
+            searchResultList.setPredicate(patient ->  {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (patient.getFirst_name().toLowerCase().indexOf(lowerCaseFilter) != -1 ) {
+                    return true;
+                }
+                else if (patient.getSecond_name().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }
+                else return false;
+
+            });
+        });
+        searchResultSortedList = new SortedList<>(searchResultList);
+        searchResultSortedList.comparatorProperty().bind(patientsTable.comparatorProperty());
+        patientsTable.setItems(searchResultSortedList);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        col_fname.setCellValueFactory(new PropertyValueFactory<Patient, String>("second_name"));
+        col_name.setCellValueFactory(new PropertyValueFactory<Patient, String>("first_name"));
+        col_bdate.setCellValueFactory(new PropertyValueFactory<Patient, Date>("dateOfbirth"));
+        getThemAll();
+        filter.setItems(comboList);
+        filter.setValue("Ophtamologue");
+    }
+}
