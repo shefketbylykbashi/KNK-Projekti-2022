5cb55236ad6e67ac254afbe562c54d4cca05ea77
Rina Bekolli <94354965+rinabekolli@users.noreply.github.com>
2022-05-22 23:10:52 +0200

    wip

---
+++ KNK-Projekti-2022/src/model/AppConsultationLogic.java
@@ -0,0 +1,11 @@
+package model;
+
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class AppConsultationLogic {
+    public static LocalDate localDate = LocalDate.of(2022, 05, 27);
+    public static Date date =  Date.valueOf(localDate);
+    public static Patient person = new Patient("name","last name",date);
+}
