867394ab245411a537d77653dd777d45ee142c60
festinaimeraj <93921647+festinaimeraj@users.noreply.github.com>
2022-05-23 17:53:33 +0200

    Update ModifyPatientController.java

--- KNK-Projekti-2022/src/controllers/ModifyPatientController.java
+++ KNK-Projekti-2022/src/controllers/ModifyPatientController.java
@@ -10,4 +10,13 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 import model.Patient;
 import model.WaitingRoom;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
