5a98c034d27c9bcf528ae1848a971e5b357af4e2
festinaimeraj <93921647+festinaimeraj@users.noreply.github.com>
2022-05-23 17:52:53 +0200

    Create ModifyPatientController.java

---
+++ KNK-Projekti-2022/src/controllers/ModifyPatientController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import model.Patient;
+import model.WaitingRoom;
+
