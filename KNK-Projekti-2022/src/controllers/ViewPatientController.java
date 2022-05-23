064304c51f35b6b49cb1ad29a852c08ece43a1e0
festinaimeraj <93921647+festinaimeraj@users.noreply.github.com>
2022-05-23 17:59:23 +0200

    Create ViewPatientController.java

---
+++ KNK-Projekti-2022/src/controllers/ViewPatientController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Patient;
+import model.WaitingRoom;
