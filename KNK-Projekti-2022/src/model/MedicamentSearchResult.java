34ff2afded6ba79fb9ac093bdd390f926a9d320d
Erna Hulaj <erna.hulaj@student.uni-pr.edu>
2022-05-23 16:46:27 +0200

    create MedicamentSearchResult.java

---
+++ KNK-Projekti-2022/src/model/MedicamentSearchResult.java
@@ -0,0 +1,61 @@
+package model;
+
+public class MedicamentSearchResult
+{
+
+    private String name;
+    private String form;
+    private String dosage;
+    private boolean morning;
+    private boolean noon;
+    private boolean evening;
+
+    public MedicamentSearchResult(String name, String form, String dosage, boolean morning, boolean noon, boolean evening) {
+        this.name = name;
+        this.form = form;
+        this.dosage = dosage;
+        this.morning = morning;
+        this.noon = noon;
+        this.evening = evening;
+    }
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public boolean isMorning() {
+        return morning;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicamentSearchResult{" +
+                "name='" + name + '\'' +
+                ", form='" + form + '\'' +
+                ", dosage='" + dosage + '\'' +
+                ", morning=" + morning +
+                ", noon=" + noon +
+                ", evening=" + evening +
+                '}';
+    }
+
+    public boolean isNoon() {
+        return noon;
+    }
+
+    public boolean isEvening() {
+        return evening;
+    }
+}
