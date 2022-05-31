f122326f8f33339c6ac6d8691776497cd1fc2398
Shefketbylykbashi <shefketbylykbashi11@gmail.com>
2022-05-31 19:58:30 +0200

    wip

--- KNK-Projekti-2022/src/application/Main.java
+++ KNK-Projekti-2022/src/application/Main.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.util.ResourceBundle;
+
+import controllersAndProcessor.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import modelAndRepository.LocaleBundle;
 import modelAndRepository.WaitingRoom;
 
 
@@ -21,10 +25,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        
+    	ResourceBundle bundle =  LocaleBundle.bundle(LoginController.lang);
         WaitingRoom.patientPushedFromPatientsScene = null;
         System.setProperty("prism.lcdtext", "false");
-        Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"),bundle);
         Scene scene = new Scene(root, 1280, 720);
         Image img = new Image("/resources/Group2.png");
         
